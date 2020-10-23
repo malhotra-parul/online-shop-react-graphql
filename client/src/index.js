e9f0ec09b82e51ce76d683d8fc3b4e333ca65ad0
malhotra-parul <parulm.business@gmail.com>
2020-10-23 10:38:31 +0530

    created routing for the project

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Checkout from "./components/Checkout";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route exact component={App} path="/" />
+      <Route component={Signin} path="/signin"/>
+      <Route component={Signup} path="/signup"/>
+      <Route component={Checkout} path="/checkout"/>
+    </Switch>
+  </Router>
+)
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
